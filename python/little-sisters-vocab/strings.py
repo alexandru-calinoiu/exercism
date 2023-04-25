4fe8d5d7921dcb083ee8dfb6ffe0922471adb4ef
calin <calinoiu.alexandru@agilfreaks.com>
2023-04-25 11:39:34 +0300

    Add a couple of more python stuff

---
+++ python/little-sisters-vocab/strings.py
@@ -0,0 +1,72 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
+def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
+    """
+
+    return add_prefix(word, "un")
+
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    prefix = vocab_words[0]
+    result = prefix
+    for word in vocab_words[1:]:
+        result += " :: " + add_prefix(word, prefix)
+    return result
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+
+    adjusted_word = word.split("ness")[0]
+    if last_code_point(adjusted_word) == "i":
+        return adjusted_word[:-1] + "y"
+    return adjusted_word
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set", 2) becomes "darken".
+    """
+
+    adjective = sentence.split()[index]
+    if last_code_point(adjective) == ".":
+        adjective = adjective[:-1]
+    return adjective + "en"
+
+
+def add_prefix(word, prefix):
+    return prefix + word
+
+
+def last_code_point(word):
+    return word[-1]
